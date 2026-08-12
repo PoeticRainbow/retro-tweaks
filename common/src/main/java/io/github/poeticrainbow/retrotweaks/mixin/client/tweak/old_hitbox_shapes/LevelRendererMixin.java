c3532d13d99aa0a1b5f9c954b434fc3e94dfc6ae
PoeticRainbow <jarrett.bayer@outlook.com>
2026-08-12 13:28:14 -0400

    update old outlines tweak for extraction separation

--- common/src/main/java/io/github/poeticrainbow/retrotweaks/mixin/client/tweak/old_hitbox_shapes/LevelRendererMixin.java
+++ common/src/main/java/io/github/poeticrainbow/retrotweaks/mixin/client/tweak/old_hitbox_shapes/LevelRendererMixin.java
@@ -3,7 +3,7 @@ package io.github.poeticrainbow.retrotweaks.mixin.client.tweak.old_hitbox_shapes
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import io.github.poeticrainbow.retrotweaks.util.OldBlockShapesHelper;
-import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.extract.LevelExtractor;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.At;
  *
  * @author PoeticRainbow
  */
-@Mixin(LevelRenderer.class)
+@Mixin(LevelExtractor.class)
 public abstract class LevelRendererMixin {
     @WrapOperation(method = "extractBlockOutline", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;"))
     private VoxelShape retrotweaks$override_block_outline(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context, Operation<VoxelShape> original) {
