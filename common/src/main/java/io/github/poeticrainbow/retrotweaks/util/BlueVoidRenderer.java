1bdc0baca8e97bc8115633d82c5749775673f095
PoeticRainbow <jarrett.bayer@outlook.com>
2026-08-15 09:19:16 -0400

    implement blue void tweak on 26.1

--- common/src/main/java/io/github/poeticrainbow/retrotweaks/util/BlueVoidRenderer.java
+++ common/src/main/java/io/github/poeticrainbow/retrotweaks/util/BlueVoidRenderer.java
@@ -2,17 +2,20 @@ package io.github.poeticrainbow.retrotweaks.util;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.textures.GpuTextureView;
+import io.github.poeticrainbow.retrotweaks.mixin.client.required.LevelRendererAccessor;
 import io.github.poeticrainbow.retrotweaks.mixin.client.required.SkyRendererAccessor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.util.ARGB;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
-import java.util.Optional;
 import java.util.OptionalDouble;
+import java.util.OptionalInt;
 
 public class BlueVoidRenderer {
     public static void renderBlueVoid(int skyColor) {
-        var skyRendererAccessor = (SkyRendererAccessor) Minecraft.getInstance().levelRenderer.skyRenderer();
+        var skyRendererAccessor = (SkyRendererAccessor) ((LevelRendererAccessor) Minecraft.getInstance().levelRenderer).retrotweaks$getSkyRenderer();
 
         if (skyRendererAccessor != null) {
             var modelViewStack = RenderSystem.getModelViewStack();
@@ -24,17 +27,17 @@ public class BlueVoidRenderer {
                                 .mul(0.2F, 0.2F, 0.6F, 1.0F)
                                 .add(0.04F, 0.04F, 0.1F, 0.0F);
 
-            var dynamicTransforms = RenderSystem.getDynamicUniforms().writeTransform(modelViewStack, voidColor);
+            var dynamicTransforms = RenderSystem.getDynamicUniforms().writeTransform(modelViewStack, voidColor, new Vector3f(), new Matrix4f());
 
-            GpuTextureView colorTexture = skyRendererAccessor.retrotweaks$getRenderTarget().getColorTextureView();
-            GpuTextureView depthTexture = skyRendererAccessor.retrotweaks$getRenderTarget().getDepthTextureView();
+            GpuTextureView colorTexture = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
+            GpuTextureView depthTexture = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
 
-            try (var renderPass = RenderSystem.getDevice().createCommandEncoder().createRenderPass(() -> "RetroTweaks Blue Void", colorTexture, Optional.empty(), depthTexture, OptionalDouble.empty())) {
+            try (var renderPass = RenderSystem.getDevice().createCommandEncoder().createRenderPass(() -> "RetroTweaks Blue Void", colorTexture, OptionalInt.empty(), depthTexture, OptionalDouble.empty())) {
                 renderPass.setPipeline(RenderPipelines.SKY);
                 RenderSystem.bindDefaultUniforms(renderPass);
                 renderPass.setUniform("DynamicTransforms", dynamicTransforms);
-                renderPass.setVertexBuffer(0, skyRendererAccessor.retrotweaks$getBottomSkyBuffer().slice());
-                renderPass.draw(10, 1, 0, 0);
+                renderPass.setVertexBuffer(0, skyRendererAccessor.retrotweaks$getBottomSkyBuffer());
+                renderPass.draw(0, 10);
             }
 
             modelViewStack.popMatrix();
