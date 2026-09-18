86796a2e51781f3a48c06991d0460405f9a639ee
PoeticRainbow <jarrett.bayer@outlook.com>
2026-09-18 17:09:37 -0400

    add neoforge mods menu support

--- neoforge/src/main/java/io/github/poeticrainbow/retrotweaks/neoforge/RetroTweaksNeoForge.java
+++ neoforge/src/main/java/io/github/poeticrainbow/retrotweaks/neoforge/RetroTweaksNeoForge.java
@@ -3,18 +3,19 @@ package io.github.poeticrainbow.retrotweaks.neoforge;
 import dev.architectury.platform.Platform;
 import io.github.poeticrainbow.retrotweaks.RetroTweaks;
 import io.github.poeticrainbow.retrotweaks.TweakPlatform;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(RetroTweaks.MOD_ID)
 public final class RetroTweaksNeoForge implements TweakPlatform {
-    public RetroTweaksNeoForge() {
+    public RetroTweaksNeoForge(ModContainer container) {
         // Run our common setup.
         RetroTweaks.PLATFORM = this;
         // the platform MUST be set before initializing for config loading
         RetroTweaks.init();
 
         if (Platform.getEnv().isClient()) {
-            RetroTweaksNeoForgeClient.init();
+            RetroTweaksNeoForgeClient.init(container);
         }
     }
 
